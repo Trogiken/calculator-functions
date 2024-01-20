89865acedd678b0902ad231f88f4ab9391bcc8a5
Noah <70231827+Trogiken@users.noreply.github.com>
2024-01-20 00:04:06 -0500

    Create dyn-nav.js

---
+++ docs/dyn-nav.js
@@ -0,0 +1,13 @@
+document.getElementById('shared-nav-container').innerHTML = `
+<nav>
+  <ul>
+    <li><a href="introduction.html">Introduction</a></li>
+    <li><a href="installation.html">Installation</a></li>
+    <li><a href="usage.html">Usage</a></li>
+    <li><a href="examples.html">Examples</a></li>
+    <li><a href="api-reference.html">API Reference</a></li>
+    <li><a href="faq.html">FAQ</a></li>
+    <li><a href="contributing.html">Contributing</a></li>
+  </ul>
+</nav>
+`;
